2b93ad380761d4eafb8dffec0d5ad24278cb5064
Pavel94 <zhilinsky.pa@yandex.ru>
2023-10-14 11:16:36 +0300

    renamed the operator chooser

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -7,12 +7,12 @@ def current_game():
     number1 = random.randint(1, 10)
     number2 = random.randint(1, 10)
     operator = ['+', '-', '*']
-    op = random.choice(operator)
-    question = f'{number1} {op} {number2}'
-    if '+' == op:
+    get_operator = random.choice(operator)
+    question = f'{number1} {get_operator} {number2}'
+    if '+' == get_operator:
         right_answer = str(number1 + number2)
-    if '-' == op:
+    if '-' == get_operator:
         right_answer = str(number1 - number2)
-    if "*" == op:
+    if "*" == get_operator:
         right_answer = str(number1 * number2)
     return (question, right_answer)
