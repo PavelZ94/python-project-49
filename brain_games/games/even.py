c41b452a637cfe2df5d79a32a9ff6a98eef1db72
Pavel94 <zhilinsky.pa@yandex.ru>
2023-10-19 22:49:11 +0300

    add modules in brain-even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -3,11 +3,14 @@ import random
 
 INTRO = 'Answer "yes" if the number is even, otherwise answer "no".'
 
-
-def current_game():
-    question = random.randint(1, 100)
+def calculate(question):
     if question % 2 == 0:
         right_answer = 'yes'
     else:
         right_answer = 'no'
+    return(right_answer)
+
+def current_game():
+    question = random.randint(1, 100)
+    right_answer = calculate(question)
     return (question, right_answer)
