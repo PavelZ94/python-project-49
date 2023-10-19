8f7dcc9b8f39a408a26971f38152ff3ebcba4f3a
Pavel94 <zhilinsky.pa@yandex.ru>
2023-10-19 22:44:18 +0300

    adding modules

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -2,6 +2,15 @@ import random
 
 INTRO = 'What is the result of the expression?'
 
+def calculate(number1, number2, get_operator):
+    if '+' == get_operator:
+        right_answer = str(number1 + number2)
+    if '-' == get_operator:
+        right_answer = str(number1 - number2)
+    if "*" == get_operator:
+        right_answer = str(number1 * number2)
+    return(right_answer)
+
 
 def current_game():
     number1 = random.randint(1, 10)
@@ -9,10 +18,5 @@ def current_game():
     operator = ['+', '-', '*']
     get_operator = random.choice(operator)
     question = f'{number1} {get_operator} {number2}'
-    if '+' == get_operator:
-        right_answer = str(number1 + number2)
-    if '-' == get_operator:
-        right_answer = str(number1 - number2)
-    if "*" == get_operator:
-        right_answer = str(number1 * number2)
+    right_answer = calculate(number1, number2, get_operator)
     return (question, right_answer)
