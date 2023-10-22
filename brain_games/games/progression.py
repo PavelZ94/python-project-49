a688c51211aae247bcf09d5b65b1fb3835ccc7e3
Pavel94 <zhilinsky.pa@yandex.ru>
2023-10-22 22:19:21 +0300

    progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,18 +3,18 @@ import random
 INTRO = 'What number is missing in the progression?'
 
 
-def make_question(chosen_number, progression):
-    new_progression = [x if x != chosen_number else ".." for x in progression]
+def make_question(number1, number2, period):
+    progression = list(range(number1, number2, period))
+    right_answer = random.choice(progression)
+    new_progression = [x if x != right_answer else ".." for x in progression]
     question = (" ".join(map(str, new_progression)))
-    return question
+    return (question, right_answer)
 
 
 def current_game():
     number1 = random.randint(1, 5)
     number2 = random.randint(17, 20)
     period = random.randint(1, 3)
-    progression = list(range(number1, number2, period))
-    chosen_number = random.choice(progression)
-    question = make_question(chosen_number, progression)
-    right_answer = str(chosen_number)
+    question, right_answer = make_question(number1, number2, period)
+    right_answer = str(right_answer)
     return (question, right_answer)
